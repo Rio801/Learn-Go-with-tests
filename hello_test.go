38c16d826e93439ed0f82641263e1fe8b5d99b2f
Rio801 <latonyajmusic@gmail.com>
2025-06-29 13:08:58 -0600

    editing the test

--- hello_test.go
+++ hello_test.go
@@ -3,10 +3,23 @@ package main
 import "testing"
 
 func TestHello(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+	t.Run("Testing for Hello, name return", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+		assertCorrectMessage(t, got, want)
+	})
 
+	t.Run("Test for Hello World return", func(t *testing.T) {
+		got := Hello("World")
+		want := "Hello, World"
+
+		assertCorrectMessage(t, got, want)
+	})
+}
+
+func assertCorrectMessage(t testing.TB, got, want string) {
+	t.Helper()
 	if got != want {
-		t.Errorf("got:  %q, want: %q ", got, want)
+		t.Errorf("got: %q ,Wanted: %q", got, want)
 	}
 }
